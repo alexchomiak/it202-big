203f6cc879407347ac089865c46703d8eabdccc3
alexchomiak <alex.chomiak@gmail.com>
2019-04-10 01:32:38 -0500

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# link to project planning document
-https://docs.google.com/document/d/1Akya8pXrUWCs6d7cQnKtVdTfPh3TyUJ-CqsUB1vbZYg/edit?usp=sharing
---
+++ precache-manifest.024670fb7c772a520f3630cdb8df2fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5987091e411d5a938227",
+    "url": "/static/css/main.06fd83be.chunk.css"
+  },
+  {
+    "revision": "5987091e411d5a938227",
+    "url": "/static/js/main.d0357e9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9e7dc599d281a07a58f5",
+    "url": "/static/js/2.d7088209.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "84b0f569ad477777f9be72580c7580ea",
+    "url": "/index.html"
+  }
+];
