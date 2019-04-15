f3677b871b1d6792ca20e3075245bc5eb79cdde7
alexchomiak <alex.chomiak@gmail.com>
2019-04-15 08:55:25 -0500

    added build screen beta

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="header">
-      <img class="headerlogo" src="/spotify.png"></img>
+      <img class="headerlogo" src={process.env.PUBLIC_URL + "/spotify.png"}></img>
       <div>
       <h1 class="header-title"> People the DJ</h1>
       <p class="header-subtitle">The people have spoken. Let them curate your playlists.</p>
